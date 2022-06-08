b9660fce5ac0032ff4f69437654a21004e0d07a0
Gabriel <98906548+Gabriel200126@users.noreply.github.com>
2022-06-08 11:20:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ function menuClick(){
 gsap.from('ul li',{ opacity:0, x:-20, duration:1.5,  stagger:0.5 })
 
 gsap.from('.work, .name, .hello', {opacity:0, duration:1.5,scale:0.5,  stagger:1 })
+
+
+let subm = document.querySelector('.feedback-input')
+
+function sub(){
+    alert('thanks for visiting my website' + ' ' + subm.value);
+}
