7affa0fe9462ca925accc01ee8f12d9a8401b936
Gabriel <98906548+Gabriel200126@users.noreply.github.com>
2022-06-08 10:25:55 +0200

    Add files via upload

---
+++ data.php
@@ -0,0 +1,18 @@
+<?php
+
+
+if($_POST["Hello"]) {
+
+
+mail("gabriel.dumitru2123@gmail.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: raga.gafa35@gmail.com");
+
+
+}
+
+
+?>
+   
+   
